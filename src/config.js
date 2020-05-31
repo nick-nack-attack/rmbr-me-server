95b2838a27e135375269de6908b1172a2ae7db7a
Nick Farni <nickfarni001@gmail.com>
2020-05-30 18:53:09 -0600

    endpoints to login working

--- src/config.js
+++ src/config.js
@@ -5,6 +5,7 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN,
-    JWT_EXPIRY: process.env.JWT_EXPIRY
+    JWT_EXPIRY: process.env.JWT_EXPIRY,
+    API_TOKEN: process.env.API_TOKEN,
 }
 
