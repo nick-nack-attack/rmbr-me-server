d0bacfba753b26c2e5c5bf43eb3f79f7a2eb1f8f
Nick Nack Attack <nickfarni001@gmail.com>
2022-04-08 15:39:36 -0600

    change from commonjs to module

--- src/middleware/basic-auth.ts
+++ src/middleware/basic-auth.ts
@@ -1,24 +1,23 @@
-const AuthService = require('../auth/auth-service')
+import AuthService  from '../auth/auth-service';
 
-function requireAuth(req, res, next) {
+export default function requireAuth(req, res, next) {
   const authToken = req.get('Authorization') || '';
   let basicToken;
+
   if (!authToken.toLowerCase().startsWith('basic ')) {
     return res.status(401).json({error: 'Missing basic token'})
   } else {
     basicToken = authToken.slice('basic '.length, authToken.length)
   }
-  ;
+
   const [tokenUsername, tokenPassword] = AuthService.parseBasicToken(basicToken);
+
   if (!tokenUsername || !tokenPassword) {
     return res.status(401).json({error: 'Unauthorized request'})
   }
-  ;
-  AuthService.getUserWithUsername(
-      req.app.get('db'),
-      tokenUsername
-  )
-      .then(user => {
+
+  AuthService.getUserWithUsername(tokenUsername)
+    .then(user => {
         if (!user) {
           return res.status(401).json({error: 'Unauthorized request'})
         }
@@ -32,8 +31,4 @@ function requireAuth(req, res, next) {
             })
       })
       .catch(next)
-};
-
-module.exports = {
-  requireAuth
 };
