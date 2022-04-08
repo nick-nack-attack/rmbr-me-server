c616539a7a3e86eae0203eca31a3c9cfac32c1b9
Nick Nack Attack <nickfarni001@gmail.com>
2022-04-08 16:13:24 -0600

    remove error being thrown when it doesn't need to

--- src/middleware/add-db.ts
+++ src/middleware/add-db.ts
@@ -3,9 +3,6 @@ import { isConnected } from "../database/connect";
 import { db } from '../database/connect'
 
 export function addDbMiddleware(req: Request, res: Response, next: NextFunction) {
-    if (!isConnected) {
-        return next(Error('database has not yet connected'));
-    }
     req['db'] = db;
     next();
 }
