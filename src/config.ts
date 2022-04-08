b6d1e55b57fd53d9ec18ac6ec85b330b2db333e4
Nick Nack Attack <nickfarni001@gmail.com>
2022-04-08 15:39:40 -0600

    change from commonjs to module

--- src/config.ts
+++ src/config.ts
@@ -3,22 +3,11 @@ import * as dotenv from 'dotenv';
 
 dotenv.config({path: path.join(__dirname, '../.env')});
 
-const PORT = process.env.PORT;
-const NODE_ENV = process.env.NODE_ENV;
-const DATABASE_URL = process.env.DATABASE_URL;
-const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
-const JWT_SECRET = process.env.JWT_SECRET;
-const JWT_EXPIRY = +process.env.JWT_EXPIRY;
-const API_TOKEN = process.env.API_TOKEN;
-const ADMIN_PERMISSION = process.env.ADMIN_PERMISSION;
-
-export {
-  PORT,
-  NODE_ENV,
-  DATABASE_URL,
-  TEST_DATABASE_URL,
-  JWT_SECRET,
-  JWT_EXPIRY,
-  API_TOKEN,
-  ADMIN_PERMISSION,
-};
+export const PORT = process.env.PORT;
+export const NODE_ENV = process.env.NODE_ENV;
+export const DATABASE_URL = process.env.DATABASE_URL;
+export const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+export const JWT_SECRET = process.env.JWT_SECRET;
+export const JWT_EXPIRY = +process.env.JWT_EXPIRY;
+export const API_TOKEN = process.env.API_TOKEN;
+export const ADMIN_PERMISSION = process.env.ADMIN_PERMISSION;
